608758aa1b605f4c8d03fe057bbf2c843661323f
Hussein-Sultani <hussein.sultani187@gmail.com>
2022-10-09 15:01:06 +1100

    Start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import SignUp from './components/SignUp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
+
